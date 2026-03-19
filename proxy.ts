b68da91ade5b86323d000034513e1dd9f5cf3ed2
fraol2728-ops <fraol2728@gmail.com>
2026-03-19 14:21:55 +0300

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*), "/lessons(.*)""]);
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/lessons(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
